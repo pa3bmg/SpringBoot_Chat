a69d828272ef371a94f98e68f13c13f793170602
Ton Smink <pa3bmg@home.nl>
2018-02-07 22:11:53 +0100

    Update Schedular tasks

---
+++ src/main/java/nl/pa3bmg/chat/config/SchedulerConfig.java
@@ -0,0 +1,30 @@
+package nl.pa3bmg.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/*
+ * By default, all the @Scheduled tasks are executed in a default thread pool of size one created by Spring.
+ * But hey, You can create your own thread pool and configure Spring to use that thread pool 
+ * for executing all the scheduled tasks.
+ * Create a new package config inside com.example.schedulerdemo, and then create a new class called 
+ * SchedulerConfig inside config package with the following contents -
+ */
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+	private final int POOL_SIZE = 10;
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("my-scheduled-task-pool-");
+        threadPoolTaskScheduler.initialize();
+
+        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
+}
--- src/main/java/nl/pa3bmg/chat/scheduledtaks/ScheduledTasks.java
+++ src/main/java/nl/pa3bmg/chat/scheduledtaks/ScheduledTasks.java
@@ -1,7 +1,9 @@
 package nl.pa3bmg.chat.scheduledtaks;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +18,31 @@ public class ScheduledTasks {
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
+    		log.info("reportCurrentTime Current Thread : {}", Thread.currentThread().getName());
         log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+    
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {
+    	log.info("scheduleTaskWithFixedDelay Current Thread : {}", Thread.currentThread().getName());
+        log.info("Fixed Delay Task :: Execution Time - {}", dateFormat.format(new Date()));
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            log.error("Ran into an error {}", ex);
+            throw new IllegalStateException(ex);
+        }
+    }
+    
+    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+    public void scheduleTaskWithInitialDelay() {
+    		log.info("scheduleTaskWithInitialDelay Current Thread : {}", Thread.currentThread().getName());
+        log.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateFormat.format(new Date()));
+    }
+    
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduleTaskWithCronExpression() {
+    		log.info("scheduleTaskWithCronExpression Current Thread : {}", Thread.currentThread().getName());
+        log.info("Cron Task :: Execution Time - {}", dateFormat.format(new Date()));
     }
 }
